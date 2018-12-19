8e5d68ed30a81d8d882ba455eebc4b2f0a64a729
Thomas Gardner <thomasgardner@orthofix.com>
2018-12-19 08:10:58 -0600

    Minor fixes

--- FOAASApp.ts
+++ FOAASApp.ts
@@ -1,39 +1,39 @@
 import {
-    IConfigurationExtend, IEnvironmentRead, ILogger,
-  } from '@rocket.chat/apps-engine/definition/accessors';
-  import { App } from '@rocket.chat/apps-engine/definition/App';
-  import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
-  import { SettingType } from '@rocket.chat/apps-engine/definition/settings';
-  
-  import { FOASSCommand } from './commands/FOAASCommand';
-  
-  export class FOAAS extends App {
-    constructor(info: IAppInfo, logger: ILogger) {
-      super(info, logger);
-    }
-  
-    protected async extendConfiguration(configuration: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
-      await configuration.settings.provideSetting({
-        id: 'foass_name',
-        type: SettingType.STRING,
-        packageValue: 'foass',
-        required: true,
-        public: false,
-        i18nLabel: 'Customize_Name',
-        i18nDescription: 'Customize_Name_Description',
-      });
-  
-      await configuration.settings.provideSetting({
-        id: 'foass_icon',
-        type: SettingType.STRING,
-        packageValue: 'https://raw.githubusercontent.com/tgardner851/Rocket.Chat.App-foass/master/icon.jpg',
-        required: true,
-        public: false,
-        i18nLabel: 'Customize_Icon',
-        i18nDescription: 'Customize_Icon_Description',
-      });
-  
-      await configuration.slashCommands.provideSlashCommand(new FOASSCommand());
-    }
+  IConfigurationExtend, IEnvironmentRead, ILogger,
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { SettingType } from '@rocket.chat/apps-engine/definition/settings';
+
+import { FOAASCommand } from './commands/FOAASCommand';
+
+export class FOAASApp extends App {
+  constructor(info: IAppInfo, logger: ILogger) {
+    super(info, logger);
   }
+
+  protected async extendConfiguration(configuration: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
+    await configuration.settings.provideSetting({
+      id: 'foaas_name',
+      type: SettingType.STRING,
+      packageValue: 'foass',
+      required: true,
+      public: false,
+      i18nLabel: 'Customize_Name',
+      i18nDescription: 'Customize_Name_Description',
+    });
+
+    await configuration.settings.provideSetting({
+      id: 'foaas_icon',
+      type: SettingType.STRING,
+      packageValue: 'https://raw.githubusercontent.com/tgardner851/Rocket.Chat.App-foaas/master/icon.jpg',
+      required: true,
+      public: false,
+      i18nLabel: 'Customize_Icon',
+      i18nDescription: 'Customize_Icon_Description',
+    });
+
+    await configuration.slashCommands.provideSlashCommand(new FOAASCommand());
+  }
+}
   
--- commands/FOAASCommand.ts
+++ commands/FOAASCommand.ts
@@ -1,47 +1,41 @@
 import { IHttp, IModify, IRead } from '@rocket.chat/apps-engine/definition/accessors';
 import { ISlashCommand, SlashCommandContext } from '@rocket.chat/apps-engine/definition/slashcommands';
 
-export class FOASSCommand implements ISlashCommand {
-    public command: string = 'foass';
+export class FOAASCommand implements ISlashCommand {
+    public command: string = 'foaas';
     public i18nParamsExample: string = 'Slash_Command_Params_Example';
     public i18nDescription: string = 'Slash_Command_Description';
     public providesPreview: boolean = false;
 
     public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp): Promise<void> {
-        const icon = await read.getEnvironmentReader().getSettings().getValueById('fuckingweather_icon');
-        const username = await read.getEnvironmentReader().getSettings().getValueById('fuckingweather_name');
+        const icon = await read.getEnvironmentReader().getSettings().getValueById('foaas_icon');
+        const username = await read.getEnvironmentReader().getSettings().getValueById('foaas_name');
 
         let text = 'Fuck this error!';
         let successful = false;
 
         let url = 'http://foaas.com/';
 
-        const from = '@' + context.getSender().name; // Attempt to mention
+        const from = '@' + context.getSender().username; // Attempt to mention
         // Check for <object> or not
         const object = context.getArguments().slice().join(' ');
-        if (object !== null && object !== undefined) {
-          let options = ['off', 'you', 'donut', 'shakespeare', 'linus', 'king', 'chainsaw', 'outside', 'madison', 'nugget', 'yoda'];
-          options.push(object);
-          const random_fu = options[Math.floor(Math.random()*options.length)];
-          if (random_fu === object) {
-            url += random_fu + '/' + from + '/';
-          } else {
-            url += random_fu + '/' + object + '/' + from + '/';
-          }
+        if (object !== null && object !== undefined && object.trim() !== '') {
+          const options = ['off', 'you', 'donut', 'shakespeare', 'linus', 'king', 'chainsaw', 'outside', 'madison', 'nugget', 'yoda'];
+          const randomFu = options[Math.floor(Math.random() * options.length)];
+          url += randomFu + '/' + object + '/' + from + '/';
         } else {
           const options = ['this', 'that', 'everything', 'everyone', 'pink', 'life', 'thanks', 'flying', 'fascinating', 'cool', 'what', 'because'];
-          const random_fu = options[Math.floor(Math.random()*options.length)];
-          url += random_fu + "/" + from + "/";
+          const randomFu = options[Math.floor(Math.random() * options.length)];
+          url += randomFu + '/' + from + '/';
         }
 
-        const response = await http.get(url, { headers: { Accept: 'application/json', 'User-Agent': 'Mozilla/5.0' } }); // Plz no blacklist
+        const response = await http.get(url, { headers: { 'Accept': 'application/json', 'User-Agent': 'Mozilla/5.0' } }); // Plz no blacklist
         if (response && response.content) {
-          try{
+          try {
             const json = JSON.parse(response.content);
             text = json.message + '\n' + json.subtitle;
             successful = true;
-          }
-          catch(err){
+          } catch (err) {
             successful = false;
             text = 'Fuck this parsing error!';
           }
